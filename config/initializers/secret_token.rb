ac47db8bbf2ad4e15ef1a4693db46157f499220b
Roberto <rmartin.martin@gmail.com>
2012-01-31 01:51:31 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Docsocial::Application.config.secret_token = '4498cb405a8e0326c3efc8a99c549f0a006f3bd3af12eb29ae05666c59c117f22864ea61bfa4480bc90156221677c8d77bee39fb7712be7ee3373e982e189a1b'
