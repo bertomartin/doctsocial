3de13e5d514523bf0f57e7cc75a154555cea6884
Roberto <rmartin.martin@gmail.com>
2012-03-01 23:07:50 -0500

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,9 +26,7 @@ namespace :deploy do
 end
 
 #after 'deploy:update_code', 'deploy:symlink_shared'
-#after 'deploy:update_code' do 
-#  run "cd #{release_path}; RAILS_ENV=production rake assets:precompile"
-#end
+
 
 
 
